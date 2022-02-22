421f0fa1727cef9e05c4b07cd0f4f89479d63046
USER <yeji1919@gmail.com>
2022-02-22 16:17:43 +0900

    helmet/eslint/prettier 설정

--- Root.js
+++ Root.js
@@ -1,19 +1,19 @@
 // express 모듈 가져오기
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // Csp
-const helmet = require('helmet')
-app.use(helmet())
-app.disable('x-powered-by')
+const helmet = require("helmet");
+app.use(helmet());
+app.disable("x-powered-by");
 
 // 포트번호 지정
-const port = 3000
+const port = 3000;
 
-const todoRouter = require('./TodoRouter')
+const todoRouter = require("./TodoRouter");
 
-app.use('/todo', todoRouter)
+app.use("/todo", todoRouter);
 
 app.listen(port, () => {
-    console.log('Test ${port}')
-})
+  console.log("Test ${port}");
+});
