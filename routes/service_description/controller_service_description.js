cf163222bd3244848a6ccd4717037e878abf48b8
USER <yeji1919@gmail.com>
2022-02-25 06:38:18 +0900

    상태코드 추가

--- routes/service_description/controller_service_description.js
+++ routes/service_description/controller_service_description.js
@@ -2,9 +2,7 @@
 
 // 해당 라우터에서 get 요청을 받았을 때
 const get_service_description = function (req, res) {
-  res.send("서비스설명");
-
-  console.log("/service_description");
+  res.status(200).send("서비스설명화면");
 };
 
 // 모듈화
--- routes/service_description/service_description.js
+++ routes/service_description/service_description.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller_service_description");
 
-// 요청 별 정의
+// 서비스설명 탭 화면
 router.get("/", controller.get_service_description);
 
 // 모듈화
