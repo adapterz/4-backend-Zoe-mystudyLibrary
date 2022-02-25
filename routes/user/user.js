d6e40acafaaf777bd145694c34c7921004890ecd
USER <yeji1919@gmail.com>
2022-02-25 12:34:20 +0900

    비밀번호 변경 router

--- routes/user/user.js
+++ routes/user/user.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller_user");
 // 요청 별 정의
+// 내정보 기본화면
 router.get("/", controller.get_user);
-
+// 비밀번호변경창
+router.get("/new_pw", controller.get_revise_pw);
+// 비밀번호변경요청
+router.patch("new_pw", controller.patch_revise_pw);
 // 모듈화
 module.exports = router;
