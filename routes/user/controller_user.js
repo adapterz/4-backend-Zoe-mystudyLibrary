42a4ca15ba06fd2b95f1f979a4af8840983a98dd
USER <yeji1919@gmail.com>
2022-02-25 12:32:56 +0900

    모델 객체 추가/비밀번호 변경 api 추가

--- routes/user/controller_user.js
+++ routes/user/controller_user.js
@@ -1,11 +1,33 @@
 // 내 정보 라우터의 컨트롤러
+// 모델 객체
+const model_user = require("/model/user");
 
 // 해당 라우터에서 get 요청을 받았을 때(기본 화면)
 const get_user = function (req, res) {
-  res.send("내정보창이에요");
+  res.status(200).send("내정보창");
+};
+
+// 비밀번호 수정(get)
+const get_revise_pw = function (req, res) {
+  res.status(200).send("비밀번호변경창");
+};
+
+// 비밀번호 수정(patch)
+const patch_revise_pw = function (req, res) {
+  // 입력된 비밀번호 정보 가져오기
+  const new_user_data = req.body;
+  // 비밀번호 변경
+  model_user.revise_pw(
+    new_user_data.pw.toString(),
+    new_user_data.confirm_pw.toString()
+  );
+  // 상태코드
+  res.send(200).send("비밀번호 변경 성공");
 };
 
 // 모듈화
 module.exports = {
   get_user: get_user,
+  get_revise_pw: get_revise_pw,
+  patch_revise_pw: patch_revise_pw,
 };
