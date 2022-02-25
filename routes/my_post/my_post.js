5d79f426e43b81197226a1c322fdfbdcd917fcbf
USER <yeji1919@gmail.com>
2022-02-26 05:33:44 +0900

    내가작성한글/댓글/후기

--- routes/my_post/my_post.js
+++ routes/my_post/my_post.js
@@ -4,8 +4,18 @@ const router = express.Router();
 const controller = require("./controller_mypost");
 
 // 요청 별 정의
-// 기본화면
-router.get("/", controller.my);
+// 내가 작성한 글
+router.get("/post", controller.my_post);
+// 선택글 삭제
+router.delete("/post", controller.delete_my_post);
+// 내가 작성한 댓글
+router.get("/comments", controller.my_comment);
+// 선택댓글 삭제
+router.delete("/comments", controller.delete_my_post);
+// 내가 작성한 후기
+router.get("/epilogue", controller.my_epilogue);
+// 해당후기 삭제
+router.delete("/epilogue", controller.delete_my_epilogue);
 
 // 모듈화
 module.exports = router;
