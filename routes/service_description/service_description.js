b5a01c31b0379965253bcc2cbf7b1680cc8074cc
USER <yeji1919@gmail.com>
2022-02-28 09:01:48 +0900

    아직 작성할 내용이 없습니다

--- routes/service_description/service_description.js
+++ routes/service_description/service_description.js
@@ -3,8 +3,5 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller_service_description");
 
-// 서비스설명 탭 화면
-router.get("/", controller.get_service_description);
-
 // 모듈화
 module.exports = router;
