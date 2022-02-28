521990f9e54f8cb16bceb905c5944a4250b29cb5
USER <yeji1919@gmail.com>
2022-02-28 17:54:32 +0900

    내 주변 도서관 라우터

--- router/adj_lib.js
+++ router/adj_lib.js
@@ -2,9 +2,29 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller_adj_lib");
+// 유효성 검사를 위한 모듈
+const { body } = require("express-validator");
 
-// 요청 별 정의
-router.get("/", controller.get_adj_lib);
+// 전체도서관 정보
+router.get("/", controller.allLib);
+// 내 지역의 도서관 정보(시도명, 시군구명 body로 보내기)
+router.post(
+  "/",
+  body("nameOfCity")
+    .isString()
+    .trim()
+    .notEmpty()
+    .isLength({ max: 10 })
+    .matches(/^[가-힣]+$/), // 한글만
+  body("districts")
+    .isString.trim()
+    .notEmpty()
+    .isLength({ max: 10 })
+    .matches(/^[가-힣]+$/),
+  controller.localLib,
+);
+// 특정 도서관 자세히 보기
+router.get("/:lib-index", controller.particularLib);
 
 // 모듈화
 module.exports = router;
