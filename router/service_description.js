06b5fafb318ad900dad460305ce2c1fed7fb1763
USER <yeji1919@gmail.com>
2022-02-28 16:56:54 +0900

    주석 추가

--- router/service_description.js
+++ router/service_description.js
@@ -2,6 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller_service_description");
-
+// 아직 작성할거 따로x -> 기능이 없는 페이지
 // 모듈화
 module.exports = router;
