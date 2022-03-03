fef4e5339ea4752f75c1df43688bf72b57d990b8
USER <yeji1919@gmail.com>
2022-03-04 03:08:36 +0900

    prettier

--- a_mymodule/db.js
+++ a_mymodule/db.js
@@ -1,6 +1,6 @@
 // DB 연결을 위한 모듈 설치 및 DB connection 정보 설정
 const mysql = require("mysql");
-const colors = require("./colors");
+const colors = require("colors");
 
 const db_info = {
   host: process.env.DB_HOST,
@@ -8,12 +8,13 @@ const db_info = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  multipleStatements: true, // 다중쿼리 허용
 };
 // DB 커넥션 생성 및 DB 접속
 const db_connect = mysql.createConnection(db_info);
 db_connect.connect(function (err) {
-  if (err) console.error(colors.error("mysql error:" + err));
-  else console.log(colors.info("mysql이 성공적으로 연결됐습니다."));
+  if (err) console.error(("mysql error:" + err).red);
+  else console.log("mysql 이 성공적으로 연결됐습니다.".gray.italic);
 });
 
 // 모듈화
