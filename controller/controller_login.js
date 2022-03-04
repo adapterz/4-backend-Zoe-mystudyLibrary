40c8b96cca2eaa8b06fac8061ec4e5c29508794a
USER <yeji1919@gmail.com>
2022-03-04 13:09:12 +0900

    DB 연동 코드 새로작성/ 안쓰는 예시 지워주기

--- controller/controller_login.js
+++ controller/controller_login.js
@@ -1,78 +1,37 @@
 // 로그인화면의 라우터의 컨트롤러
 // 예시 유저 정보
 const crypto = require("crypto");
-const users = [
-  {
-    userIndex: 1,
-    id: "syjg1234",
-    pw: "hassing_pw1",
-    name: "Zoe",
-    gender: "Woman",
-    phoneNumber: "01028400631",
-    salt: "1234#",
-    nickName: null,
-    profileShot: null,
-  },
-  {
-    userIndex: 2,
-    id: "ye1919",
-    password: "hassing_pw2",
-    name: "Yeji",
-    gender: "Woman",
-    phoneNumber: "01128400631",
-    salt: "1234!",
-    nickName: null,
-    profileShot: null,
-  },
-  {
-    userIndex: 3,
-    id: "hihi123",
-    password: "hassing_pw3",
-    name: "Leehi",
-    gender: "Man",
-    phoneNumber: "01234567890",
-    salt: "12a13",
-    nickName: null,
-    profileShot: null,
-  },
-];
+const db = require("../a_mymodule/db");
+const moment = require("../a_mymodule/date_time");
 
 // 로그인
 const login = function (req, res) {
-  /* 예시 정보
-  body = {
-  id: "syjg1234",
-  pw: "st123456#"
-  }
-   */
-
   // 유저가 입력한 정보 가져오기
   const input_login = req.body;
-
-  // 입력한 아이디와 DB의 아이디들과 대조
-  let is_existed = false;
-  let user_index = -1;
-  for (const index in users) {
-    if (input_login.id === users[index].id) {
-      is_existed = true;
-      user_index = index;
-      break;
+  const query = "SELECT id,pw,name,gender,phoneNumber,salt,nickName,profileShot,userLib FROM USER WHERE id = ?";
+  // 쿼리문 실행
+  db.db_connect.query(query, function (err, results) {
+    if (err) {
+      console.log(("login 메서드 mysql 모듈사용 실패:" + err).red.bold);
+      return res.status(500).json({ state: "login 메서드 mysql 모듈사용 실패:" + err });
     }
-  }
-  // 유효성 검사
-  // 1. 존재하는 아이디가 없을 때
-  if (!is_existed) return res.status(400).json({ state: "유효하지 않은 데이터입니다." });
-  // 입력한 비밀번호 해싱
-  const salts = users[user_index].salt;
-  const hash_pw = crypto
-    .createHash("sha512")
-    .update(input_login.pw + salts)
-    .digest("hex");
-  // 2. 등록된 유저 pw와 입력한 pw가 다르면 로그인 실패
-  if (hash_pw !== users[user_index].pw) return res.status(400).json({ state: "유효하지 않은 데이터입니다." });
+    console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
 
-  // 로그인 성공
-  return res.status(200).json(users[user_index]);
+    // 1. 존재하는 아이디가 없을 때
+    if (results[0].id !== input_login) return res.status(400).json({ state: "존재하는 id가 없습니다." });
+    const salts = results[0].salt;
+    // 입력한 비밀번호 해싱
+    const hash_pw = crypto
+      .createHash("sha512")
+      .update(input_login.pw + salts)
+      .digest("hex");
+
+    // 2. 등록된 유저 pw와 입력한 pw가 다르면 로그인 실패
+    if (hash_pw !== results[0].pw) return res.status(400).json({ state: "비밀번호가 일치하지 않습니다." });
+
+    // 로그인 성공
+    return res.status(200).json(results);
+  });
 };
 
 // 모듈화
