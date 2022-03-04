fb2cfad0f7d51bd8dd0d4cf8b095f4097eef1938
USER <yeji1919@gmail.com>
2022-03-05 00:26:16 +0900

    새 DB연동 메서드 작성

--- controller/controller_user.js
+++ controller/controller_user.js
@@ -3,6 +3,7 @@
 const crypto = require("crypto");
 const db = require("../a_mymodule/db");
 const moment = require("../a_mymodule/date_time");
+const mysql = require("mysql");
 const user = {
   userIndex: 1,
   id: "syjg1234",
@@ -156,7 +157,45 @@ const dropOut = function (req, res) {
 
 // TODO
 // 내 관심도서관(adj_lib 코드 작성 후 구현)
+const myLib = function (req, res) {
+  // 로그인이 안 돼있을 때
+  if (user.id === null) return res.status(401).json({ state: "인증되지 않은 사용자입니다." });
+  // 내 관심도서관 문자열 가져오기 ex. myLib = 늘푸른도서관index(숫자);123123;3634523;3432; <이런형태
+  let myLib_string;
+  let query = "SELECT myLib FROM USER WHERE id=? ";
+  // 쿼리문 실행
+  db.db_connect.query(query, [user.id], function (err, results) {
+    if (err) {
+      console.log(("myLib 메서드 mysql 모듈사용 실패:" + err).red.bold);
+      return res.status(500).json({ state: "myLib 메서드 mysql 모듈사용 실패:" + err });
+    }
+    console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
+    myLib_string = results;
+  });
+  let myLib_split = myLib_string.split(";");
+  // WHERE 조건문에 파싱한 libIndex 조건들 추가해주기
+  query =
+    "SELECT libIndex,libName,libType,closeDay,timeWeekday,timeSaturday,timeHoliday,grade,address,libContact,nameOfCity,districts FROM LIBRARY WHERE libIndex=";
+  let split_query = mysql.escape(myLib_split[0]);
+  const arr_size = myLib_split.length;
+  for (let cnt = 1; cnt < arr_size; ++cnt) {
+    split_query += "OR libIndex =";
+    split_query += mysql.escape(myLib_split[cnt]);
+  }
+  query += mysql.escape(split_query);
 
+  // 내가 관심있어하는 도서관 정보보기
+  db.db_connect.query(query, function (err, results) {
+    if (err) {
+      console.log(("myLib 메서드 mysql 모듈사용 실패:" + err).red.bold);
+      return res.status(500).json({ state: "myLib 메서드 mysql 모듈사용 실패:" + err });
+    }
+    console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
+
+    // 성공적으로 리턴
+    return res.status(200).json(results);
+  });
+};
 // TODO 회원탈퇴를 제외한 DELETE문 Update문 deleteDate = 날짜/시간 쿼리문으로 변경
 
 // 모듈화
