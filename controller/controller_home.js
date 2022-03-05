b2fbb697a5780b0e56d58dcfacd7b917b0a29cb2
USER <yeji1919@gmail.com>
2022-03-06 04:30:04 +0900

    오류수정시도

--- controller/controller_home.js
+++ controller/controller_home.js
@@ -30,8 +30,9 @@ const getRecentPost = function (req, res) {
 const myLib = function (req, res) {
   // 로그인이 안 돼있을 때
   if (user.id === null) return res.status(401).json({ state: "인증되지 않은 사용자입니다." });
-  // 내 관심도서관 문자열 가져오기 ex. myLib = 늘푸른도서관index(숫자);123123;3634523;3432; <이런형태
+  // 내 관심도서관 문자열 가져오기 ex. userLib = ;늘푸른도서관index(숫자);123123;3634523;3432; <이런형태
   let myLib_string;
+  let myLib_split;
   let query = "SELECT userLib FROM USER WHERE id=? ";
   // 쿼리문 실행
   db.db_connect.query(query, [user.id], function (err, results) {
@@ -40,20 +41,21 @@ const myLib = function (req, res) {
       return res.status(500).json({ state: "myLib 메서드 mysql 모듈사용 실패:" + err });
     }
     console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
-    // myLib_string = results;
-  });
 
-  let myLib_split = myLib_string.split(";");
-  // WHERE 조건문에 파싱한 libIndex 조건들 추가해주기
-  query =
-    "SELECT libIndex,libName,libType,closeDay,timeWeekday,timeSaturday,timeHoliday,grade,address,libContact,nameOfCity,districts FROM LIBRARY WHERE libIndex=";
-  let split_query = mysql.escape(myLib_split[0]);
-  const arr_size = myLib_split.length;
-  for (let cnt = 1; cnt < arr_size; ++cnt) {
-    split_query += "OR libIndex =";
-    split_query += mysql.escape(myLib_split[cnt]);
-  }
-  query += mysql.escape(split_query);
+    myLib_string = results[0].userLib;
+    myLib_split = myLib_string.split(";");
+    let split_query = mysql.escape(myLib_split[0]);
+    const arr_size = myLib_split.length;
+    for (let cnt = 1; cnt < arr_size; ++cnt) {
+      split_query += "OR libIndex =";
+      split_query += mysql.escape(myLib_split[cnt]);
+    }
+    // WHERE 조건문에 파싱한 libIndex 조건들 추가해주기
+    query =
+      "SELECT libIndex,libName,libType,closeDay,timeWeekday,timeSaturday,timeHoliday,grade,address,libContact,nameOfCity,districts FROM LIBRARY WHERE libIndex=";
+    query += mysql.escape(split_query);
+    console.log(query);
+  });
 
   // 내가 관심있어하는 도서관 정보보기
   db.db_connect.query(query, function (err, results) {
