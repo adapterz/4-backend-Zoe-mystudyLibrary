69355d441b82da9daf5866cb7f9140577afe69d9
USER <yeji1919@gmail.com>
2022-03-06 02:30:36 +0900

    디버깅용 콘솔

--- a_mymodule/validation.js
+++ a_mymodule/validation.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 const is_validate = function (req, res, next) {
   const errors = validationResult(req);
   // 유효하지 않음
+  console.log(errors);
   if (!errors.isEmpty()) return res.status(400).json({ state: "유효하지 않은 데이터 입니다." });
 
   next();
