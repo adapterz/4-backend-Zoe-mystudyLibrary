6d29a71891fd8d5e37f445310e5321a6618d7b60
USER <yeji1919@gmail.com>
2022-03-09 04:17:43 +0900

    경로명 변경

--- router/my_post.js
+++ router/my_post.js
@@ -5,18 +5,18 @@ const controller = require("../controller/controller_mypost");
 
 // 요청 별 정의
 // 내가 쓴 글 데이터 목록 가져오기
-router.get("/post", controller.myPost);
+router.get("/my-post", controller.myPost);
 // 내가 쓴 댓글 데이터 목록 가져오기
-router.get("/comments", controller.myComment);
+router.get("/my-comments", controller.myComment);
 // 도서관 후기 데이터 목록 가져오기
-router.get("/lib-epilogue", controller.myEpilogue);
+router.get("/lib-review", controller.myEpilogue);
 
 // 목록 중 선택 글 삭제
-router.delete("/post", controller.deletePost);
+router.delete("/my-post", controller.deletePost);
 // 목록 중 선택 댓글 삭제
-router.delete("/comments", controller.deleteComment);
+router.delete("/my-comments", controller.deleteComment);
 // 목록 중 도서관 후기 삭제
-router.delete("/lib-epilogue", controller.deleteReview);
+router.delete("/lib-review", controller.deleteReview);
 
 // 모듈화
 module.exports = router;
