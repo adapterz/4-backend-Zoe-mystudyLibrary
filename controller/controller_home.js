5f25e1d1bc192fac77079e46206cd37499d03200
USER <yeji1919@gmail.com>
2022-03-09 02:42:31 +0900

    홈화면/관심도서관 DB변동사항 적용

--- controller/controller_home.js
+++ controller/controller_home.js
@@ -6,14 +6,14 @@ const mysql = require("mysql");
 const user = {
   id: "Zoe",
   nickName: "Zoe",
-  userIndex: 1312,
+  userIndex: 123123,
 };
 // 최신글 정보가져오기
 const getRecentPost = function (req, res) {
   // 최신글 자유게시판 글 5개/공부인증샷 글 4개 불러오기
   const query =
-    "SELECT postTitle,nickName,hits,favorite FROM BOARDS WHERE category = ? order by boardIndex DESC limit 5;" +
-    "SELECT postTitle,nickName,hits,favorite FROM BOARDS WHERE category = ? order by boardIndex DESC limit 4;";
+    "SELECT postTitle,nickName,hits,favorite FROM BOARDS LEFT JOIN USER ON BOARDS.userIndex=user.userIndex WHERE BOARDS.deleteDate IS NULL AND category = ? order by boardIndex DESC limit 5;" +
+    "SELECT postTitle,nickName,hits,favorite FROM BOARDS LEFT JOIN USER ON BOARDS.userIndex=user.userIndex WHERE BOARDS.deleteDate IS NULL AND category = ? order by boardIndex DESC limit 4;";
   db.db_connect.query(query, ["자유게시판", "공부인증샷"], function (err, results) {
     // 오류발생
     if (err) {
@@ -29,43 +29,18 @@ const getRecentPost = function (req, res) {
 // 내 관심도서관(adj_lib 코드 작성 후 구현)
 const myLib = function (req, res) {
   // 로그인이 안 돼있을 때
-  if (user.id === null) return res.status(401).json({ state: "인증되지 않은 사용자입니다." });
-  // 내 관심도서관 문자열 가져오기 ex. userLib = ;늘푸른도서관index(숫자);123123;3634523;3432; <이런형태
-  let myLib_string;
-  let myLib_split;
-  let query = "SELECT userLib FROM USER WHERE id=? ";
+  if (user.userIndex === null) return res.status(401).json({ state: "인증되지 않은 사용자입니다." });
+  // 해당 유저가 관심도서관으로 등록한 도서관 정보 가져오기
+  let query =
+    "SELECT libIndex,libName,libType,closeDay,openWeekday,endWeekday,openSaturday,endSaturday,openHoliday,endHoliday,nameOfCity,districts,address,libContact FROM library LEFT JOIN userLib ON LIBRARY.libIndex = userLib.userLib WHERE LIBRARY.deleteDate IS NULL AND userLib.deleteDate IS NULL AND userIndex=" +
+    mysql.escape(user.userIndex);
   // 쿼리문 실행
-  db.db_connect.query(query, [user.id], function (err, results) {
-    if (err) {
-      console.log(("myLib 메서드 mysql 모듈사용 실패:" + err).red.bold);
-      return res.status(500).json({ state: "myLib 메서드 mysql 모듈사용 실패:" + err });
-    }
-    console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
-
-    myLib_string = results[0].userLib;
-    myLib_split = myLib_string.split(";");
-    let split_query = mysql.escape(myLib_split[0]);
-    const arr_size = myLib_split.length;
-    for (let cnt = 1; cnt < arr_size; ++cnt) {
-      split_query += "OR libIndex =";
-      split_query += mysql.escape(myLib_split[cnt]);
-    }
-    // WHERE 조건문에 파싱한 libIndex 조건들 추가해주기
-    query =
-      "SELECT libIndex,libName,libType,closeDay,timeWeekday,timeSaturday,timeHoliday,grade,address,libContact,nameOfCity,districts FROM LIBRARY WHERE libIndex=";
-    query += mysql.escape(split_query);
-    console.log(query);
-  });
-
-  // 내가 관심있어하는 도서관 정보보기
   db.db_connect.query(query, function (err, results) {
     if (err) {
       console.log(("myLib 메서드 mysql 모듈사용 실패:" + err).red.bold);
       return res.status(500).json({ state: "myLib 메서드 mysql 모듈사용 실패:" + err });
     }
     console.log(("CLIENT IP: " + req.ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
-
-    // 성공적으로 리턴
     return res.status(200).json(results);
   });
 };
