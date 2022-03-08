35e50393e0d92ceebc83a6bdd97b7f614bb62ca8
USER <yeji1919@gmail.com>
2022-03-09 07:02:33 +0900

    커넥션풀 적용

--- a_mymodule/db.js
+++ a_mymodule/db.js
@@ -1,8 +1,8 @@
 // DB 연결을 위한 모듈 설치 및 DB connection 정보 설정
 const mysql = require("mysql");
-const colors = require("colors");
 
-const db_info = {
+const config = {
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
@@ -11,11 +11,7 @@ const db_info = {
   multipleStatements: true, // 다중쿼리 허용
 };
 // DB 커넥션 생성 및 DB 접속
-const db_connect = mysql.createConnection(db_info);
-db_connect.connect(function (err) {
-  if (err) console.error(("mysql error:" + err).red);
-  else console.log("mysql 이 성공적으로 연결됐습니다.".gray.italic);
-});
+const pool = mysql.createPool(config);
 
 // 모듈화
-module.exports = { db_connect: db_connect };
+module.exports = { db_connect: pool };
