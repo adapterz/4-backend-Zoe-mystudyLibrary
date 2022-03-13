450736ceb471ed2881b8019c26e667bb9f71ebf0
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-03-13 18:52:38 +0900

    쿼리문 성공/실패시 로그 상수화

---
+++ a_mymodule/const.js
@@ -0,0 +1,15 @@
+// 자주 사용하는 반복적인 코드 정의
+const moment = require("./date_time");
+
+// model 에서 쿼리 메서드 수행 실패시 로그 찍기 및 모델 실행 결과 반환
+export const query_fail_log = function(err){
+    if (err) {
+        console.log(("model-isAuthor 메서드 mysql 모듈사용 실패:" + err).red.bold);
+
+        return { state: "mysql 사용실패" };
+    }
+}
+// model 에서 쿼리 메서드 성공적으로 수행시 로그 찍기
+export const query_success_log = function (ip,query) {
+    console.log(("CLIENT IP: " + ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
+}
