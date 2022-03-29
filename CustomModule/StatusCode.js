5d5c176742b9c56c77f4ecf6725218f945589086
USER <yeji1919@gmail.com>
2022-03-29 12:31:41 +0900

    statudCode 코드컨벤션 적용( es6 import/export)

--- CustomModule/StatusCode.js
+++ CustomModule/StatusCode.js
@@ -1,22 +1,10 @@
 // 상태코드 상수화
-const OK = 200;
-const CREATED = 201;
-const NO_CONTENT = 204;
-const BAD_REQUEST = 400;
-const UNAUTHORIZED = 401;
-const FORBIDDEN = 403;
-const NOT_FOUND = 404;
-const CONFLICT = 409;
-const INTERNAL_SERVER_ERROR = 500;
-
-module.exports = {
-  OK: OK,
-  CREATED: CREATED,
-  NO_CONTENT: NO_CONTENT,
-  BAD_REQUEST: BAD_REQUEST,
-  UNAUTHORIZED: UNAUTHORIZED,
-  FORBIDDEN: FORBIDDEN,
-  NOT_FOUND: NOT_FOUND,
-  CONFLICT: CONFLICT,
-  INTERNAL_SERVER_ERROR: INTERNAL_SERVER_ERROR,
-};
+export const OK = 200;
+export const CREATED = 201;
+export const NO_CONTENT = 204;
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+export const CONFLICT = 409;
+export const INTERNAL_SERVER_ERROR = 500;
