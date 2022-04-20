4eb780aff3873ae211728c09c800a113a59c34b5
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-04-21 05:07:09 +0900

    (성공시/실패시 )queryLog -> ModelLog로 메서드 명 변경

--- CustomModule/CheckDataOrAuthority.js
+++ CustomModule/CheckDataOrAuthority.js
@@ -3,7 +3,7 @@
 import mysql from "mysql2/promise";
 // 내장모듈
 import { myPool } from "./Db";
-import { queryFailLog, querySuccessLog } from "./QueryLog";
+import { modelFailLog, modelSuccessLog } from "./QueryLog";
 
 // 삭제/수정 요청시 해당 게시글의 존재유무 체크, 해당 게시글의 작성자인지 체크 하는 함수
 export async function checkBoardMethod(boardIndex, userIndex, ip) {
@@ -12,7 +12,7 @@ export async function checkBoardMethod(boardIndex, userIndex, ip) {
   // 성공시
   try {
     let [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 요청한 게시글이 존재하지 않을 때
     if (results[0] === undefined) {
       return { state: "존재하지않는게시글" };
@@ -25,7 +25,7 @@ export async function checkBoardMethod(boardIndex, userIndex, ip) {
       "AND userIndex=" +
       mysql.escape(userIndex);
     [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 해당 게시글의 작성자와 요청유저가 일치하지 않을 때
     if (results[0] === undefined) {
       return { state: "접근권한없음" };
@@ -34,7 +34,7 @@ export async function checkBoardMethod(boardIndex, userIndex, ip) {
     return { state: "접근성공" };
     // 쿼리문 실행시 에러발생
   } catch (err) {
-    await queryFailLog(err, ip, query);
+    await modelFailLog(err, ip, query);
     return { state: "mysql 사용실패" };
   }
 }
@@ -47,7 +47,7 @@ export async function checkCommentMethod(boardIndex, commentIndex, userIndex, is
   try {
     // 게시글이 존재하는지 확인할 쿼리문 실행
     let [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 게시글이 존재하지 않을 때
     if (results[0] === undefined) {
       return { state: "존재하지않는게시글" };
@@ -61,7 +61,7 @@ export async function checkCommentMethod(boardIndex, commentIndex, userIndex, is
         "AND commentIndex=" +
         mysql.escape(commentIndex);
       [results, fields] = await myPool.query(query);
-      await querySuccessLog(ip, query);
+      await modelSuccessLog(ip, query);
       // 댓글이 존재하지 않을 때
       if (results[0] === undefined) {
         return { state: "존재하지않는댓글" };
@@ -76,7 +76,7 @@ export async function checkCommentMethod(boardIndex, commentIndex, userIndex, is
         mysql.escape(userIndex);
       // 쿼리문 실행
       [results, fields] = await myPool.query(query);
-      await querySuccessLog(ip, query);
+      await modelSuccessLog(ip, query);
       // 해당 댓글의 작성자와 요청유저가 일치하지 않을 때
       if (results[0] === undefined) {
         return { state: "접근권한없음" };
@@ -85,7 +85,7 @@ export async function checkCommentMethod(boardIndex, commentIndex, userIndex, is
     return { state: "접근성공" };
     // 쿼리문 실행시 에러발생
   } catch (err) {
-    await queryFailLog(err, ip, query);
+    await modelFailLog(err, ip, query);
     return { state: "mysql 사용실패" };
   }
 }
@@ -99,7 +99,7 @@ export async function checkReviewMethod(libraryIndex, reviewIndex, userIndex, ip
   try {
     // 쿼리문 실행
     let [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 도서관 정보가 존재하지 않을 때
     if (results[0] === undefined) {
       return { state: "존재하지않는도서관" };
@@ -108,7 +108,7 @@ export async function checkReviewMethod(libraryIndex, reviewIndex, userIndex, ip
     query = "SELECT userIndex FROM REVIEW WHERE deleteTimestamp IS NULL AND reviewIndex=" + mysql.escape(reviewIndex);
     [results, fields] = await myPool.query(query);
     // 성공로그
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 해당 후기가 존재하지 않을 때
     if (results[0] === undefined) {
       return { state: "존재하지않는후기" };
@@ -121,7 +121,7 @@ export async function checkReviewMethod(libraryIndex, reviewIndex, userIndex, ip
       mysql.escape(userIndex);
     [results, fields] = await myPool.query(query);
     // 성공로그
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 해당 후기의 작성자와 요청유저가 일치하지 않을 때
     if (results[0] === undefined) {
       return { state: "접근권한없음" };
@@ -130,7 +130,7 @@ export async function checkReviewMethod(libraryIndex, reviewIndex, userIndex, ip
     return { state: "접근성공" };
     // 쿼리문 실행시 에러발생
   } catch (err) {
-    await queryFailLog(err, ip, query);
+    await modelFailLog(err, ip, query);
     return { state: "mysql 사용실패" };
   }
 }
@@ -144,7 +144,7 @@ export async function checkUserLibraryMethod(libraryIndex, userIndex, ip) {
   try {
     // 쿼리문 실행
     let [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 관심도서관 등록한 적이 없을 때
     if (results[0] === undefined) {
       return { state: "존재하지않는도서관" };
@@ -156,7 +156,7 @@ export async function checkUserLibraryMethod(libraryIndex, userIndex, ip) {
       " AND libraryIndex=" +
       mysql.escape(libraryIndex);
     [results, fields] = await myPool.query(query);
-    await querySuccessLog(ip, query);
+    await modelSuccessLog(ip, query);
     // 성공로그
     // 기존에 관심도서관으로 등록되지 않았을 때
     if (results[0] === undefined) {
@@ -165,7 +165,7 @@ export async function checkUserLibraryMethod(libraryIndex, userIndex, ip) {
     return { state: "접근성공" };
     // 쿼리문 실행시 에러발생
   } catch (err) {
-    await queryFailLog(err, ip, query);
+    await modelFailLog(err, ip, query);
     return { state: "mysql 사용실패" };
   }
 }
--- CustomModule/QueryLog.js
+++ CustomModule/QueryLog.js
@@ -1,13 +1,30 @@
-// 쿼리문 성공/실패 여부 로그
+// sequelize 를 사용한 Model 메서드 성공/실패 여부 로그
 import { moment } from "./DateTime";
 // Model 에서 쿼리 메서드 수행 실패시 로그 찍기 및 모델 실행 결과 반환
-export async function queryFailLog(err, ip, query) {
+export async function modelFailLog(err, ip, method) {
   console.log(
-    ("mysql 모듈사용 실패:" + err + "\nCLIENT IP: " + ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query)
-      .red.bold,
+    (
+      "sequelize 모듈사용 실패:" +
+      err +
+      "\nCLIENT IP: " +
+      ip +
+      "\nDATETIME: " +
+      moment().format("YYYY-MM-DD HH:mm:ss") +
+      "\nMETHOD: " +
+      method
+    ).red.bold
   );
 }
 // Model 에서 쿼리 메서드 성공적으로 수행시 로그 찍기
-export async function querySuccessLog(ip, query) {
-  console.log(("CLIENT IP: " + ip + "\nDATETIME: " + moment().format("YYYY-MM-DD HH:mm:ss") + "\nQUERY: " + query).blue.bold);
+export async function modelSuccessLog(ip, method) {
+  console.log(
+    (
+      "sequelize 모듈사용 성공 \nCLIENT IP: " +
+      ip +
+      "\nDATETIME: " +
+      moment().format("YYYY-MM-DD HH:mm:ss") +
+      "\nMETHOD: " +
+      method
+    ).blue.bold
+  );
 }
