28dcefc43baaa86a34fd3ea992d29dc655843805
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-04-22 06:26:17 +0900

    controller : 유저 프로필 닉네임/이미지 모델 분리, 유저 정보(닉네임, 프로필 사진 경로) 가져오기 api 모델 작성

--- Controller/User.js
+++ Controller/User.js
@@ -18,8 +18,10 @@ import {
   deleteUserLibraryModel,
   dropOutModel,
   editPhoneNumberModel,
-  editProfileModel,
+  editProfileImageModel,
+  editProfileNicknameModel,
   editPwModel,
+  getUserModel,
   loginModel,
   registerUserLibraryModel,
   signUpModel,
@@ -35,6 +37,7 @@ import { checkUserLibraryMethod } from "../CustomModule/CheckDataOrAuthority";
  * 3. 관심도서관 조회/등록/탈퇴
  * 4. 유저가 작성한 글/댓글/후기 조회
  * 5. 유저 정보 수정
+ * 6. 유저 정보 가져오기
  *
  * 참고: Model 메서드에 인자로 보낸 요청한 유저의 ip 정보는 Model 수행 로그 남기는데 이용
  */
@@ -424,11 +427,10 @@ export async function userReviewController(req, res) {
 }
 
 // 5. 유저 정보 수정
-// 5-1. 유저 프로필 수정
-export async function editProfileController(req, res) {
+// 5-1. 유저 프로필 - 닉네임 수정
+export async function editProfileNicknameController(req, res) {
   /*
    * req.body
-   *  profileImage: 프로필 사진 uri
    *  nickname: 닉네임
    */
   try {
@@ -444,8 +446,8 @@ export async function editProfileController(req, res) {
     // payload의 유저인덱스와 signature의 유저인덱스 비교 (조작여부 확인)
     if (loginIndex !== payloadIndex) return res.status(FORBIDDEN).json({ state: "접근 권한이 없습니다." });
 
-    // 프로필 수정 요청 모델 실행결과
-    const modelResult = await editProfileModel(req.body, req.ip, loginIndex);
+    // 프로필 닉네임 수정 요청 모델 실행결과
+    const modelResult = await editProfileNicknameModel(req.body, req.ip, loginIndex);
 
     // 실행결과에 따라 분기처리
     // sequelize query 메서드 실패
@@ -467,7 +469,44 @@ export async function editProfileController(req, res) {
   }
 }
 
-// 5-2. 회원정보 수정(연락처 수정)
+// 5-2. 유저 프로필 - 이미지 수정
+export async function editProfileImageController(req, res) {
+  /*
+   * req.body
+   *  profileImage: 프로필로 지정할 사진 파일 -> multer 모듈로 인해 req.file 에 image 정보 들어감
+   */
+  try {
+    //  필요 변수 선언
+    const loginToken = req.signedCookies.token;
+    const loginIndex = await jwt.verify(loginToken, process.env.TOKEN_SECRET).idx;
+    const imagePath = "profileImage/" + req.file.filename;
+    // 업로드 요청한 파일이 이미지 형식이 아닐 때
+    if (req.body.fileValidation === false)
+      return res.status(BAD_REQUEST).json({ state: "json, jpeg, gjf, png 형식의 파일만 업로드 가능합니다." });
+    // 프로필 수정 요청 모델 실행결과
+    const modelResult = await editProfileImageModel(imagePath, req.ip, loginIndex);
+
+    // 실행결과에 따라 분기처리
+    // sequelize query 메서드 실패
+    if (modelResult.state === "sequelize 사용실패") return res.status(INTERNAL_SERVER_ERROR).json(modelResult);
+    // 성공적으로 프로필 변경
+    else if (modelResult.state === "프로필 사진 수정") return res.status(OK).end();
+    // 이미지 파일이 정상적으로 등록
+    return res.status(OK).json({ state: "프로필 사진 수정" });
+  } catch (err) {
+    // 만료된 토큰
+    if (err.message === "jwt expired") {
+      return res.status(UNAUTHORIZED).json({ state: "만료된 토큰입니다." });
+    }
+    // 유효하지 않은 토큰일 때
+    if (err.message === "invalid signature") {
+      return res.status(FORBIDDEN).json({ state: "올바르지 않은 접근입니다." });
+    }
+    return res.status(FORBIDDEN).json({ state: "접근 권한이 없습니다." });
+  }
+}
+
+// 5-3. 회원정보 수정(연락처 수정)
 export async function editPhoneNumberController(req, res) {
   /*
    * req.body
@@ -505,7 +544,7 @@ export async function editPhoneNumberController(req, res) {
   }
 }
 
-// 5-3. 비밀번호 수정
+// 5-4. 비밀번호 수정
 export async function editPwController(req, res) {
   /*
    * req.body
@@ -513,8 +552,8 @@ export async function editPwController(req, res) {
    *  newPw: 새 비밀번호
    *  confirmPw: 새 비밀번호 확인
    */
-  //  필요 변수 선언
   try {
+    //  필요 변수 선언
     const loginToken = req.signedCookies.token;
     let loginIndex;
     // 로그인 토큰이 없을 때
@@ -536,6 +575,47 @@ export async function editPwController(req, res) {
     else if (modelResult.state === "비밀번호/비밀번호확인 불일치") return res.status(BAD_REQUEST).json(modelResult);
     // 성공적으로 비밀번호 변경 요청 수행
     else if (modelResult.state === "비밀번호변경성공") return res.status(OK).json(modelResult);
+  } catch (err) {
+    // 만료된 토큰
+    if (err.message === "jwt expired") {
+      return res.status(UNAUTHORIZED).json({ state: "만료된 토큰입니다." });
+    }
+    // 유효하지 않은 토큰일 때
+    if (err.message === "invalid signature") {
+      return res.status(FORBIDDEN).json({ state: "올바르지 않은 접근입니다." });
+    }
+    return res.status(FORBIDDEN).json({ state: "접근 권한이 없습니다." });
+  }
+}
+
+// 6. 유저 정보 가져오기
+export async function getUserController(req, res) {
+  try {
+    //  필요 변수 선언
+    const loginToken = req.signedCookies.token;
+    let loginIndex;
+    // 로그인 토큰이 없을 때
+    if (loginToken === undefined)
+      return res.status(UNAUTHORIZED).json({ state: "해당 서비스 이용을 위해서는 로그인을 해야합니다." });
+    // 로그인했을 때 토큰의 유저인덱스 불러오기
+    loginIndex = await jwt.verify(loginToken, process.env.TOKEN_SECRET).idx;
+    const payloadIndex = await jwt.decode(loginToken).idx;
+    // payload의 유저인덱스와 signature의 유저인덱스 비교 (조작여부 확인)
+    if (loginIndex !== payloadIndex) return res.status(FORBIDDEN).json({ state: "접근 권한이 없습니다." });
+    // 비밀번호 수정 모델 실행결과
+    const modelResult = await getUserModel(req.ip, loginIndex);
+    // 실행결과에 따라 분기처리
+    // sequelize query 메서드 실패
+    if (modelResult.state === "sequelize 사용실패") return res.status(INTERNAL_SERVER_ERROR).json(modelResult);
+    // 프로필 사진 이름(프로필 사진 등록한 유저인덱스)과 요청 유저 인덱스가 일치하지 않을 때
+    else if (modelResult.state === "올바르지 않은 접근입니다.") return res.status(BAD_REQUEST).json(modelResult);
+    // 성공적으로 유저정보 가져옴
+    else if (modelResult.state === "유저 정보") {
+      // 등록된 프로필 사진이 없을 때
+      if (!modelResult.dataOfUser.isProfileImage) return res.status(OK).json(modelResult.dataOfUser);
+      // 등록된 프로필 사진이 있을 때
+      return res.status(OK).json(modelResult.dataOfUser);
+    }
   } catch (err) {
     // 만료된 토큰
     if (err.message === "jwt expired") {
