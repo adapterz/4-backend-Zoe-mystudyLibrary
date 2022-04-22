605b713fc20a7f239979345ccfb209dd45a2308e
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-04-22 21:00:04 +0900

    eslintrc.js 추가

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    node: true,
+    es2021: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
