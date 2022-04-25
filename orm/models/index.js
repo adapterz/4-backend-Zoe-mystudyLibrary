978c97d07ee9811a7fc56028a9f5c8433643957a
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-04-26 04:07:45 +0900

    commonJs에서 사용할 수 있었지만 es6에서는 사용 못하는 것들 사용할 수 있게 코드 작성

--- orm/models/index.js
+++ orm/models/index.js
@@ -3,39 +3,46 @@
 import fs from "fs";
 import path from "path";
 import Sequelize from "sequelize";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const basename = path.basename(__filename);
-import { development } from "../../ormConfig";
+import { development } from "../../ormConfig.js";
 export const db = {};
 
 // 시퀄라이즈 객체
 export const sequelize = new Sequelize(development.database, development.username, development.password, {
-    host: development.host,
-    dialect: development.dialect,
-    port: development.port,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000, // 오류 발생 전 연결 시도하는 최대 시간 (밀리초)
-        idle: 10000, // 10초간 사용되지 않은 풀 연결 제거 (밀리초)
-    },
-    logging: false,
+  host: development.host,
+  dialect: development.dialect,
+  port: development.port,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000, // 오류 발생 전 연결 시도하는 최대 시간 (밀리초)
+    idle: 10000, // 10초간 사용되지 않은 풀 연결 제거 (밀리초)
+  },
+  logging: false,
 });
 
 // 모델(테이블) 읽기
+
 fs.readdirSync(__dirname)
-    .filter((file) => {
-        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
-    })
-    .forEach((file) => {
-        console.log(file);
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
+  })
+  .forEach((file) => {
+    console.log(file);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
